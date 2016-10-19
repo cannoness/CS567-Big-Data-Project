6972c95d7c622a3e20c42688dffe16870a6585fd
justindonthomas <justin.don.thomas@gmail.com>
2016-10-19 16:09:41 -0600

    Building interval timer.  TODO make custom timer class

--- Tweet Search/TimelineGrabber.py
+++ Tweet Search/TimelineGrabber.py
@@ -7,11 +7,53 @@ class TimelineGrabber():
     """
     Instances of this class grab a users timeline and writes out then sleeps for 15
     minutes.
+    TODO make custom timer
     """
 
     def __init__(self):
         """
-        Constructor creates an instance of the class.  Instantiates timer.
+        Constructor instatiates class.
+        Members:
+        minutesSinceLast.  Number of minutes since last tick.
         """
         self.minutesSinceLast = 15
+        self.clock = None
+        self.isDone = False
+        self.numGrabs = 0
+        self.maxGrabs = 5 #temporary for testing right now.
         
+    def startTimer(self):
+        """
+        Start the timer.
+        """
+        self.clockTick()
+        
+    def clockTick(self):
+        """
+        This method gets called when there is a clock tick.  Timeline grabber decides if
+        it is time to grab again or prints heartbeat.
+        """
+ 
+        
+        if self.minutesSinceLast == 15:
+            #grab timelines
+            self.minutesSinceLast = 0
+            self.numGrabs += 1
+            self.checkIfDone()
+            print "Grabbing timelines"
+        else:
+            self.minutesSinceLast += 1
+            print self.minutesSinceLast, " minutes since last grab."
+
+        #reset timer and restart.
+        if not self.isDone:
+            self.clock = Timer(2.0, self.clockTick)
+            self.clock.start()
+
+    def checkIfDone(self):
+        """
+        Method for checking if done testing timer.  Kill the thread by setting
+        isDone member variable.
+        """
+        if self.numGrabs > self.maxGrabs:
+            self.isDone = True
--- Tweet Search/grabTimelines.py
+++ Tweet Search/grabTimelines.py
@@ -1,22 +1,9 @@
 import sys
-import loginScripts
-from threading import Timer
+from TimelineGrabber import TimelineGrabber
 
 def beginGrabbing():
-    clock = Timer(60.0, getLines)
     #grab 300 timelines,
     #write out
     #sleep 15 min
-
-def clockTick():
-    #perform action on clock tick.
-    
-def getLines(userList):
-    """
-    Get user timelines.
-    """
-    #assume list is 300 long.
-    for user in userList:
-        #get timeline
-        #write out
-    
+    grabber = TimelineGrabber()
+    grabber.startTimer()
