cc652270f9c6bd886eb842ae482e02ef601b0980
cannoness <jacobya@unm.edu>
2016-11-30 21:05:37 -0700

    mostly done combiner
    needs slightly more polishing but otherwise done

--- TweetContext/combiner.py
+++ TweetContext/combiner.py
@@ -41,9 +41,9 @@ for row in sent_array:
     else:
        #start new user
        #first total the old stuff and compile it into one sheet
-       user_sent_averaged.append([last_user,clinton/(trump+clinton),[s[1] for s in pol_array if s[0]==last_user]])
+       user_sent_averaged.append([last_user,clinton/(trump+clinton),[s[1] for s in pol_array if s[0]==last_user][0]])
        clinton = trump = 0
-       user_sent_first_only.append([current_user,row[1],[s[1] for s in pol_array if s[0]==current_user]])
+       user_sent_first_only.append([current_user,row[1],[s[1] for s in pol_array if s[0]==current_user][0]])
        #add to count
        if float(row[1]) == 1:
            clinton+=1
@@ -53,3 +53,13 @@ for row in sent_array:
        
 #have to append the last user
 user_sent_averaged.append([current_user,row[1],[s[1] for s in pol_array if s[0]==current_user][0]])
+
+#save to file
+outfile = 'combined_data.csv'
+with open(outfile, 'wb') as csvfile:
+    writer = csv.writer(csvfile,  quoting=csv.QUOTE_ALL)
+    for item in range(0,len(user_sent_averaged)):
+        #Write item to outcsv
+        writer.writerow([user_sent_averaged[item][0],user_sent_first_only[item][0],
+        user_sent_averaged[item][1],user_sent_first_only[item][1],user_sent_averaged[item][2],
+        user_sent_first_only[item][2]])
